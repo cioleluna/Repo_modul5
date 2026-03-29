3bfab3be427197ebde9ca97d2c0b7518d601081d
cioleluna <preciosa.211006@gmail.com>
2026-03-29 19:37:21 +0700

    commit message

--- obj/Debug/net10.0/TP_MODUL5_103022400026.AssemblyInfo.cs
+++ obj/Debug/net10.0/TP_MODUL5_103022400026.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL5_103022400026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f7b554cbc88666e2612a3cc26da5d60e5c094cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL5_103022400026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL5_103022400026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

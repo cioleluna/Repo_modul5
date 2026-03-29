5bdbf49fdb2149bf6dbcc1df32769dede6ecab40
cioleluna <preciosa.211006@gmail.com>
2026-03-29 20:03:12 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class DataGeneric<T> 
+{
+    public T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> nim = new DataGeneric<string>("103022400026");
+        nim.PrintData();
+    }
+}
